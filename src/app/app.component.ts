395c1abcf5b01d755204fb1eca850631e5d2855f
Samuel Lorand <samuel.lorand@hotmail.com>
2025-12-01 23:55:39 -0300

    feat: produtos component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar.component';
 import { Footer } from './components/footer/footer.component';
 import { HeroComponent } from './components/hero/hero.component';
+import { ProdutosComponent } from './components/produtos/produtos.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Navbar, Footer, HeroComponent],
+  imports: [RouterOutlet, Navbar, Footer, HeroComponent, ProdutosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
