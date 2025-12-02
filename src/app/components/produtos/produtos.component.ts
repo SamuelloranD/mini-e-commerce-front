fb2b2fdd4045f8b693e6a2693570cb6ea7b4afb9
Samuel Lorand <samuel.lorand@hotmail.com>
2025-12-02 01:38:57 -0300

    feat(products): fetch products from API and display in cards

--- src/app/components/produtos/produtos.component.ts
+++ src/app/components/produtos/produtos.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { ProdutosService } from '../../services/produtos.service';
+import { IProduto } from '../../interfaces/IProduto';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-produtos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './produtos.component.html',
   styleUrl: './produtos.component.css'
 })
 export class ProdutosComponent {
 
+  produtos: IProduto[] = []
+
+  constructor(private produtoService: ProdutosService)
+  {}
+
+  ngOnInit(): void {
+    this.obterTodosCarros();
+  }
+
+  obterTodosCarros() {
+    this.produtoService.obterTodos().subscribe({
+      next: (lista) => this.produtos = lista,
+      error: (erro) => console.error(erro)
+    })
+
+  }
 }
