6328db5a17ec700976dfb43358511ceb75e21d47
Samuel Lorand <samuel.lorand@hotmail.com>
2025-12-01 23:40:09 -0300

    feat: footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar.component';
+import { Footer } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, Footer],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
